c7e156d65984eaca84a3971889dae13f5886f2f0
Ivan Kabakohlo <ivan.kabakohlo@gmail.com>
2023-01-21 22:26:17 +0200

    Add Product Details Page

---
+++ client/src/features/products/components/productGallery/productGallery.tsx
@@ -0,0 +1,7 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+
+const ProductGallery = () => {
+    return <LazyLoadImage />
+}
+
+export default ProductGallery
--- client/src/features/products/pages/productDetails/productDetails.tsx
+++ client/src/features/products/pages/productDetails/productDetails.tsx
@@ -1,3 +1,21 @@
-const ProductDetails = () => <>Product Details</>
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+import ProductGallery from '../../components/productGallery/productGallery'
+
+const ProductDetails = () => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={8}>
+                <ProductGallery />
+            </Grid>
+
+            <Grid item xs={4}>
+                <Typography variant="h3">Product Name</Typography>
+                <Typography variant="body2">Product Description</Typography>
+            </Grid>
+        </Grid>
+    )
+}
 
 export default ProductDetails
